572f08cdfb5ccc261817b7da75ce2d035af71e08
srayala554 <samanyu.rayala@gmail.com>
2025-11-04 13:02:24 -0800

    Can spawn in infinite particles and add them to the "atom" (adds them to particles list as well)

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,34 +1,48 @@
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+
 
 public class Simulation extends PApplet {
-    Particle obj1;
-    Particle obj2;
-    Particle obj3;
+    Particle obj;
+    ArrayList<Particle> particles = new ArrayList<>();
+    int counter = 0;
 
     public void settings() {
-        size(800, 800);   // set the window size
+        size(800, 800);
+        // set the window size
     }
 
 
     public void setup() {
-        obj1 = new Neutron(400, 500);
-        obj2 = new Proton(300, 500);
-        obj3 = new Electron(500, 500);
+
 
     }
 
 
     public void draw() {
         background(255);
-        obj1.draw(this);
-        obj2.draw(this);
-        obj3.draw(this);
-       // System.out.println(obj1.getX() + ", " + obj1.getY());
-       // System.out.println(obj2.getX() + ", " + obj2.getY());
-       // System.out.println(obj3.getX() + ", " + obj3.getY());
-
-
+        if (mousePressed && counter < 1 && mouseX < 267) {
+            obj = new Proton(mouseX, mouseY);
+            counter++;
+        } else if (mousePressed && counter < 1 && mouseX < 533) {
+            obj = new Neutron(mouseX, mouseY);
+            counter++;
+        } else if (mousePressed && counter < 1 && mouseX < 800) {
+            obj = new Electron(mouseX, mouseY);
+            counter++;
+        }
+        if (obj instanceof Proton || obj instanceof Neutron || obj instanceof Electron) {
+            obj.draw(this);
+            if (obj.getY() < 400 && !mousePressed && counter > 0) {
+                counter = 0;
+                particles.add(obj);
+            }
+        }
+        for (Particle particle: particles)
+            particle.draw(this);
+        System.out.println("Size: " + particles.size());
+        System.out.println("Counter: " + counter);
     }
 
     public static void main(String[] args) {
